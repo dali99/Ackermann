15b77fa5b205cd2118997b065078368b21bd1ddf
Daniel Løvbrøtte Olsen <daniel.olsen99+GitHub@gmail.com>
2015-02-23 13:22:26 +0100

    Create Python version

---
+++ Ackerman.py
@@ -0,0 +1,15 @@
+def ack(n, m):
+  "Returns Ackerman of input"
+  if m == 0:
+    return n+1
+  elif m > 0 and n == 0:
+    return ack(m - 1, n)
+  elif m > 0 and n > 0:
+    return ack(m-1, ack(m, n - 1))
+
+i = 0
+ 
+while true:
+  for x in range(9):
+    print(ack(i, x))
+  i++
