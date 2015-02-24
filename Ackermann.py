058397e06cab875f33839df10f4d78e2f76d8cf8
Atebite <mwh@halvorsenfamilien.com>
2015-02-24 15:34:45 +0100

    New looping for Python implementation

--- Ackermann.py
+++ Ackermann.py
@@ -1,15 +1,18 @@
 def ack(m, n):
-  "Returns Ackerman of input"
-  if m == 0:
-      return n + 1
-  elif m > 0 and n == 0:
-      return ack(m - 1, 1)
-  elif m > 0 and n > 0:
-      return ack(m - 1, ack(m, n - 1))
+    "Returns Ackerman of input"
+    if m == 0:
+        return n + 1
+    elif m > 0 and n == 0:
+        return ack(m - 1, 1)
+    elif m > 0 and n > 0:
+        return ack(m - 1, ack(m, n - 1))
 
-m, n = 0, 0
-while n > -1:
+i, n = 0, 0
+while i < 10:
+    m = i
     while m > -1:
-        print(ack(m, n))
-        m += 1
-    n += 1
+        print("M: " + str(m) + " N: " + str(n))
+        m -= 1
+        n += 1
+    i += 1
+    n = 0
