bfb82e9ce26ebfd1ccfdd8c11402f624f1a47e49
Joakim Pettersvold <joakimpokemeg@hotmail.no>
2015-03-11 08:52:35 +0100

    Added Java version of Ackermann function

---
+++ src/Ackermann.java
@@ -0,0 +1,34 @@
+public class Ackermann {
+	static int m = 0;
+	static int n = 0;
+	static int space = 0;
+	public static void main(String[] args){
+		
+		int i = 0;
+		while(true){
+			int m = i;
+			while(m > -1){
+				System.out.println(ackermann(m, n));
+				m++;
+				n--;
+			}
+			i++;
+			n = 0;
+		}
+	}
+	
+	public static int ackermann(int m, int n){
+		if(m == 0){
+			return n + 1;
+		}
+		if(m > 0){
+			if(n > 0){
+				return ackermann(m-1,ackermann(m,n-1));
+			}
+			else{
+				return ackermann(m-1,1);
+			}
+		}
+		return -1;
+	}
+}
