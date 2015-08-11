1166be5c4f974286d062a83fe0702023b7f76db4
Daniel Løvbrøtte Olsen <daniel.olsen99+GitHub@gmail.com>
2015-08-11 18:37:25 +0200

    I fixed your code damnit

--- Ackermann.java
+++ Ackermann.java
@@ -1,7 +1,6 @@
 public class Ackermann {
 	static int m = 0;
 	static int n = 0;
-	static int space = 0;
 	public static void main(String[] args){
 		
 		int i = 0;
@@ -21,13 +20,11 @@ public class Ackermann {
 		if(m == 0){
 			return n + 1;
 		}
-		if(m > 0){
-			if(n > 0){
-				return ackermann(m-1,ackermann(m,n-1));
-			}
-			else{
-				return ackermann(m-1,1);
-			}
+		else if(m > 0 && n == 0){
+			return ackermann(m - 1, 1);
+		}
+		else if (m > 0 && n > 0){
+			return ackermann(m - 1, ackermann(m, n - 1));
 		}
 		return -1;
 	}
