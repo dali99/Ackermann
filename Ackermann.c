2943bb00522df62aab3daf212b89e72acf1d90ed
Daniel Løvbrøtte Olsen <daniel.olsen99+GitHub@gmail.com>
2015-08-11 18:28:21 +0200

    this is what happens when you dont test you code

--- Ackermann.c
+++ Ackermann.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
-int ack(m, n)
+int ack(int m, int n)
 {
   if (m == 0)
     return n + 1;
   else if (m > 0 && n == 0)
     return ack(m - 1, 1);
   else if (m > 0 && n > 0)
-    return ack(m - 1, ack(m, n - 1))
+    return ack(m - 1, ack(m, n - 1));
 }
 
 int main()
